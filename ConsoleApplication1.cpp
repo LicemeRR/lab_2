43666b27c37dba0b7e553d939bc342f8defd1765
LicemeR <80973735+LicemeRR@users.noreply.github.com>
2022-12-28 21:38:24 +0200

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	double a, b, c;
+
+	cout << "Enter 1 number:";
+	cin >> a;
+	cout << "Enter 2 number:";
+	cin >> b;
+
+	c = a + b;
+
+	cout << c << endl;
+}
+    
+
